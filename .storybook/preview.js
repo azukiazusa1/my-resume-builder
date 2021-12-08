b7ef0229da8a14796f194b7b2a3ccb634e8aff16
azukiazusa1 <cycleippei@gmail.com>
2021-12-08 21:31:02 +0900

    first commit

---
+++ .storybook/preview.js
@@ -0,0 +1,16 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { ThemeProvider as Emotion10ThemeProvider } from "emotion-theming";
+
+const defaultTheme = createTheme(); // or your custom theme
+
+const withThemeProvider = (Story, context) => {
+  return (
+    <Emotion10ThemeProvider theme={defaultTheme}>
+      <ThemeProvider theme={defaultTheme}>
+        <Story {...context} />
+      </ThemeProvider>
+    </Emotion10ThemeProvider>
+  );
+};
+
+export const decorators = [withThemeProvider];
