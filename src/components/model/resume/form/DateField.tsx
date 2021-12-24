d572446148616083504a4e2137d4a6993be441f8
azukiazusa1 <cycleippei@gmail.com>
2021-12-24 22:15:46 +0900

    Add DateField

---
+++ src/components/model/resume/form/DateField.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { FieldProps } from './Form';
+import DateAdapter from '@mui/lab/AdapterDayjs';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import TextField from '@mui/material/TextField';
+import DatePicker from '@mui/lab/DatePicker';
+import 'dayjs/locale/ja';
+import type { Dayjs } from 'dayjs';
+
+const DateField: React.FC<FieldProps<string>> = ({ label, value, onChange }) => {
+  const handleChange = (date: Dayjs | null) => {
+    if (date) {
+      onChange(date.format('YYYY-MM-DD'));
+    } else {
+      onChange('');
+    }
+  };
+
+  return (
+    <LocalizationProvider dateAdapter={DateAdapter} locale="ja">
+      <DatePicker
+        mask="____/__/__"
+        label={label}
+        value={value}
+        clearable
+        onChange={handleChange}
+        renderInput={(params) => {
+          const { inputProps, ...other } = params;
+          return (
+            <TextField
+              fullWidth
+              error={false}
+              inputProps={{ 'data-testid': 'input', ...inputProps }}
+              {...other}
+            />
+          );
+        }}
+      />
+    </LocalizationProvider>
+  );
+};
+
+export default DateField;
