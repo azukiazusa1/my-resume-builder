757a9b9a03649f850c8283e58be91d2c9f51c209
azukiazusa1 <cycleippei@gmail.com>
2022-01-02 21:34:32 +0900

    Fix test

--- src/mokcs/handlers.ts
+++ src/mokcs/handlers.ts
@@ -7,7 +7,7 @@ const baseURL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'
 
 const handlers = [
   rest.post(`${baseURL}/api/upload`, upload.post),
-  rest.get(`/api/postcodes`, postcodes.get),
+  rest.get(`${baseURL}/api/postcodes`, postcodes.get),
 ];
 
 export default handlers;
