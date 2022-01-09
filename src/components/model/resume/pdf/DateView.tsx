c583fe049e16ef5627dfb33d63311bede7d5b27f
azukiazusa1 <cycleippei@gmail.com>
2022-01-09 15:58:09 +0900

    Add Date

---
+++ src/components/model/resume/pdf/DateView.tsx
@@ -0,0 +1,27 @@
+import { Text, View } from '@react-pdf/renderer';
+import dayjs from 'dayjs';
+import React from 'react';
+
+import Cell from './atoms/Cell';
+import Label from './atoms/Label';
+
+type Props = {
+  label: string;
+  value: string;
+};
+
+const DateView: React.VFC<Props> = ({ label, value }) => {
+  const format = (date: string) => {
+    return dayjs(date).format('YYYY年MM月DD日');
+  };
+  return (
+    <Cell>
+      <View style={{ height: 40, display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+        <Label>{label}</Label>
+        <Text>{format(value)}</Text>
+      </View>
+    </Cell>
+  );
+};
+
+export default DateView;
