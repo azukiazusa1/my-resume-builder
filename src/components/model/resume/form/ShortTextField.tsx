c7839fe1fbafe190fc2e2cab2395a9e8314b293e
azukiazusa1 <cycleippei@gmail.com>
2022-01-15 23:49:59 +0900

    short text field

--- src/components/model/resume/form/ShortTextField.tsx
+++ src/components/model/resume/form/ShortTextField.tsx
@@ -1,17 +1,21 @@
 import TextField from '@mui/material/TextField';
 import React from 'react';
 
+import { FieldProps } from '@/components/model/resume/form/Form';
+import { fieldValueSelectors } from '@/store/filedValueState';
 import { ShortTextFieldOptions } from '@/store/templateState/types';
 
-import { FieldProps } from './Form';
+const { useFieldValueItem } = fieldValueSelectors;
 
-const ShortTextField: React.FC<FieldProps<string, ShortTextFieldOptions>> = ({
+const ShortTextField: React.VFC<FieldProps<string, ShortTextFieldOptions>> = ({
   label,
-  value,
+  templateId,
+  fieldId,
   onChange,
   options = { type: 'text' },
 }) => {
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const value = useFieldValueItem(templateId, fieldId);
+  const handleChange = (event: React.FocusEvent<HTMLInputElement>) => {
     onChange(event.target.value);
   };
 
@@ -19,8 +23,8 @@ const ShortTextField: React.FC<FieldProps<string, ShortTextFieldOptions>> = ({
     <TextField
       type={options.type}
       label={label}
-      value={value}
-      onChange={handleChange}
+      defaultValue={value}
+      onBlur={handleChange}
       variant="outlined"
       fullWidth
       inputProps={{ 'data-testid': 'shortText' }}
