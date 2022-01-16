c47846cb03fcdd30a7ada602461a754f96d8917e
azukiazusa1 <cycleippei@gmail.com>
2022-01-16 09:29:51 +0900

    LongTextField

--- src/components/model/resume/form/LongTextField.tsx
+++ src/components/model/resume/form/LongTextField.tsx
@@ -1,18 +1,23 @@
 import TextField from '@mui/material/TextField';
 import React from 'react';
 
+import { fieldValueSelectors } from '@/store/filedValueState';
+
 import { FieldProps } from './Form';
 
-const ShortTextField: React.FC<FieldProps<string>> = ({ label, value, onChange }) => {
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+const { useFieldValueItem } = fieldValueSelectors;
+
+const ShortTextField: React.FC<FieldProps<string>> = ({ label, templateId, fieldId, onChange }) => {
+  const value = useFieldValueItem(templateId, fieldId);
+  const handleChange = (event: React.FocusEvent<HTMLInputElement>) => {
     onChange(event.target.value);
   };
 
   return (
     <TextField
       label={label}
-      value={value}
-      onChange={handleChange}
+      defaultValue={value}
+      onBlur={handleChange}
       variant="outlined"
       multiline
       fullWidth
